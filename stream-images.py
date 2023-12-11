f4e0cfa7c40acfcf0e52abcd5d7d0621bee8edc0
Kyle McDonald <kyle@kylemcdonald.net>
2023-12-11 20:40:30 +0900

    for accurate image timing

--- stream-images.py
+++ stream-images.py
@@ -9,7 +9,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument(
     "--input_folder", default="data/frames", help="Path to the input folder"
 )
-parser.add_argument("--fps", type=int, default=15, help="Frames per second")
+parser.add_argument("--input_fps", type=int, default=15, help="Input frames per second")
+parser.add_argument("--output_fps", type=int, default=15, help="Output frames per second")
 parser.add_argument("--port", type=int, default=5555, help="Port number")
 args = parser.parse_args()
 
@@ -17,12 +18,16 @@ context = zmq.Context()
 publisher = context.socket(zmq.PUB)
 publisher.bind(f"tcp://*:{args.port}")
 
+start_time = time.time()
+frame_number = 0
+
 try:
     file_list = os.listdir(args.input_folder)
+    fns = list(sorted(file_list))
     n_frames = len(file_list)
-
-    for i in cycle(range(n_frames)):
-        fn = f"{args.input_folder}/{i:04d}.jpg"
+    skip = args.input_fps // args.output_fps
+    for i,fn in cycle(enumerate(fns[::skip])):
+        fn = os.path.join(args.input_folder, fn)
         with open(fn, "rb") as f:
             frame = f.read()
         timestamp = int(time.time() * 1000)
@@ -30,7 +35,12 @@ try:
         packed = msgpack.packb(msg)
         publisher.send(packed)
         print(fn, end="\r")
-        time.sleep(1 / args.fps)
+        
+        next_send_time = start_time + (frame_number + 1) / args.output_fps
+        sleep_time = next_send_time - time.time()
+        if sleep_time > 0:
+            time.sleep(sleep_time)
+        frame_number += 1
 
 except KeyboardInterrupt:
     pass
