8633e580c3d971ddaded73a4f8e322d1b1facf00
Kyle McDonald <kyle@kylemcdonald.net>
2023-12-12 19:00:32 +0900

    reordering

--- collector.py
+++ collector.py
@@ -9,8 +9,8 @@ receiver.bind("tcp://0.0.0.0:5558")
 publisher = context.socket(zmq.PUB)
 publisher.bind(f"tcp://0.0.0.0:5557")
 
-# msg_buffer = {}
-# next_index = None
+msg_buffer = {}
+next_index = None
 
 print("ready for messages")
 
@@ -18,32 +18,43 @@ try:
     while True:
         msg = receiver.recv()
         unpacked = msgpack.unpackb(msg)
-        
-        # if next_index is None:
-        #     next_index = index
-        # if index == 0:
-        #     next_index = 0
-        # if index < next_index:
-        #     print("dropping", index)
-        #     continue        
-        
+        index = unpacked["index"]
+
+        if next_index is None:
+            next_index = index
+        if index == 0:
+            next_index = 0
+        if index < next_index:
+            print("dropping", index)
+            continue
+
         timestamp = unpacked["timestamp"]
         index = unpacked["index"]
         worker_id = unpacked["worker_id"]
         jpg = unpacked["jpg"]
-        
+
         latency = int(time.time() * 1000) - timestamp
-        # msg_buffer[index] = msg
-        print(f"{worker_id} {index} {latency}ms")
-        
-        packed = msgpack.packb([timestamp, index, jpg])
-        publisher.send(packed)
-        
-        # while next_index in msg_buffer:
-        #     print("sending", next_index)
-        #     publisher.send(msg_buffer[next_index])
-        #     del msg_buffer[next_index]
-        #     next_index += 1
+        msg_buffer[index] = unpacked
+        print(f"incoming: {index} #{worker_id} {latency}ms")
+
+        # publisher.send(packed) # echo mode
+
+        # ordered mode
+        while next_index in msg_buffer:
+            unpacked = msg_buffer[next_index]
+            timestamp = unpacked["timestamp"]
+            index = unpacked["index"]
+            worker_id = unpacked["worker_id"]
+            jpg = unpacked["jpg"]
+
+            latency = int(time.time() * 1000) - timestamp
+            print(f"outgoing: {index} #{worker_id} {latency}ms")
+
+            packed = msgpack.packb([timestamp, index, jpg])
+            publisher.send(packed)
+            del msg_buffer[next_index]
+            next_index += 1
+
 except KeyboardInterrupt:
     pass
 finally:
