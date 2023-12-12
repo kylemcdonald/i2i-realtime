b110671842ac954d06f0a5d86ad5a799e116a26a
Kyle McDonald <kyle@kylemcdonald.net>
2023-12-12 19:01:48 +0900

    numeric sort and dynamic fps

--- stream-images.py
+++ stream-images.py
@@ -3,6 +3,7 @@ import time
 from itertools import cycle
 import argparse
 import os
+import re
 import msgpack
 from settings_subscriber import SettingsSubscriber
 
@@ -13,9 +14,6 @@ parser.add_argument(
     help="Path to the input folder",
 )
 parser.add_argument("--input_fps", type=int, default=30, help="Input frames per second")
-parser.add_argument(
-    "--output_fps", type=int, default=10, help="Output frames per second"
-)
 parser.add_argument("--start_time", type=int, default=0, help="Start time in seconds")
 parser.add_argument("--duration", type=int, default=None, help="Duration in seconds")
 parser.add_argument("--port", type=int, default=5555, help="PUSH port number")
@@ -29,19 +27,36 @@ publisher = context.socket(zmq.PUSH)
 publisher.bind(f"tcp://0.0.0.0:{args.port}")
 
 start_time = time.time()
+start_fps = settings["fps"]
+
 start_frame_number = args.start_time * args.input_fps
 input_frame_number = start_frame_number
 output_frame_number = 0
 
+def extract_number(filename):
+    match = re.search(r'\d+', filename)
+    if match:
+        return int(match.group())
+    return None
+
+def numeric_sort(file_list):
+    return sorted(file_list, key=extract_number)
+
 try:
     file_list = os.listdir(args.input_folder)
-    fns = list(sorted(file_list))
+    fns = numeric_sort(file_list)
     end_frame_number = len(file_list)
     if args.duration:
         end_frame_number = start_frame_number + args.duration * args.input_fps
-    skip = args.input_fps // args.output_fps
 
     while True:
+        skip = args.input_fps // settings["fps"]
+        true_fps = args.input_fps // skip
+        
+        if settings["fps"] != start_fps:
+            start_time = time.time()
+            output_frame_number = 0
+    
         frames = []
         indices = []
         for i in range(settings["batch_size"]):
@@ -69,7 +84,7 @@ try:
         publisher.send(packed)
         print(fn, end="\r")
 
-        next_send_time = start_time + output_frame_number / args.output_fps
+        next_send_time = start_time + output_frame_number / true_fps
         sleep_time = next_send_time - time.time()
         if sleep_time > 0:
             time.sleep(sleep_time)
