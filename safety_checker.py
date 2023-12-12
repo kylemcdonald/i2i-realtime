193607a8b48c03347722b07fb6021e1be95d265b
Kyle McDonald <kyle@kylemcdonald.net>
2023-12-13 00:59:38 +0900

    safety for chatgpt timeouts

--- safety_checker.py
+++ safety_checker.py
@@ -1,6 +1,7 @@
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+import concurrent.futures
 
 
 class SafetyChecker:
@@ -9,15 +10,23 @@ class SafetyChecker:
         self.client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
     def __call__(self, prompt):
-        content = (
-            f"""You are a content moderation AI that determines whether user input is safe or NSFW. User input will be used to control a text-to-image generator. For any input that involves nudity, sexuality, violence, or gore, reply "unsafe". For everything else, reply "safe". Evaluate this input: "{prompt}"""
-            ""
-        )
-        response = self.client.chat.completions.create(
-            model="gpt-4-1106-preview",
-            messages=[{"role": "user", "content": content}],
-            temperature=0.0,
-            max_tokens=3,
-        )
-        safe = response.choices[0].message.content == "safe"
+        def make_api_call(content):
+            response = self.client.chat.completions.create(
+                model="gpt-4-1106-preview",
+                messages=[{"role": "user", "content": content}],
+                temperature=0.0,
+                max_tokens=3,
+            )
+            return response.choices[0].message.content
+
+        content = f'You are a content moderation AI that determines whether user input is safe or NSFW. User input will be used to control a text-to-image generator. For any input that involves nudity, sexuality, violence, or gore, reply "unsafe". For everything else, reply "safe". Evaluate this input: "{prompt}".'
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            future = executor.submit(make_api_call, content)
+            try:
+                result = future.result(timeout=5)
+            except concurrent.futures.TimeoutError:
+                return "safe"
+
+        safe = result == "safe"
         return "safe" if safe else "unsafe"
