32715048ad120189bb21f77219aff26ec2bff41b
Kyle McDonald <kyle@kylemcdonald.net>
2023-12-14 23:53:17 +0900

    better reboot

--- reboot.sh
+++ reboot.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-systemctl stop i2i-worker
 cd "$(dirname "$0")"
 source .env
-ssh $OTHER_MACHINE "systemctl stop i2i-worker && shutdown -r now"
+systemctl stop i2i-worker
+ssh $OTHER_MACHINE "systemctl stop i2i-worker"
 systemctl stop i2i-server
-shutdown -r
+sleep 5
+systemctl start i2i-worker
+ssh $OTHER_MACHINE "systemctl start i2i-worker"
+systemctl start i2i-server
--- shutdown.sh
+++ shutdown.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-systemctl stop i2i-worker
 cd "$(dirname "$0")"
 source .env
+systemctl stop i2i-worker
 ssh $OTHER_MACHINE "systemctl stop i2i-worker && shutdown now"
 systemctl stop i2i-server
 shutdown
