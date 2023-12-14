b2779d6c3c8a32a049e231879931f541ef5f73aa
Kyle McDonald <kyle@kylemcdonald.net>
2023-12-14 23:46:27 +0900

    finally

--- reboot.sh
+++ reboot.sh
@@ -2,6 +2,6 @@
 systemctl stop i2i-worker
 cd "$(dirname "$0")"
 source .env
-ssh $OTHER_MACHINE "systemctl stop i2i-worker && shutdown now"
+ssh $OTHER_MACHINE "systemctl stop i2i-worker && shutdown -r now"
 systemctl stop i2i-server
-shutdown
+shutdown -r
