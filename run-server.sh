55a2de48407581857ce983c1c13a953c933e38c9
Kyle McDonald <kyle@kylemcdonald.net>
2023-12-14 22:17:44 +0900

    wait 5 minutes to start server

--- run-server.sh
+++ run-server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sleep 60
+echo "Waiting to start..."
+sleep 300
+echo "Done waiting, starting..."
 cd "$(dirname "$0")"
 source .venv/bin/activate
 DISPLAY=:0 python app.py --mode camera --mirror --fullscreen
--- shutdown.sh
+++ shutdown.sh
@@ -2,3 +2,7 @@
 cd "$(dirname "$0")"
 printenv > env.txt
 systemctl stop i2i-worker
+source .env
+ssh $OTHER_MACHINE "systemctl stop i2i-worker && shutdown -r now"
+systemctl stop i2i-server
+shutdown -r now
