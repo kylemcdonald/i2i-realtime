20a1f789feebf3ca9d3fa9e407d6d042b3bb13b5
Kyle McDonald <kyle@kylemcdonald.net>
2024-01-12 16:49:25 +0900

    swap out compel blend with custom caching blend

--- diffusion_processor.py
+++ diffusion_processor.py
@@ -1,3 +1,4 @@
+import re
 import numpy as np
 import time
 from fixed_seed import fix_seed
@@ -56,7 +57,7 @@ class DiffusionProcessor:
             returned_embeddings_type=ReturnedEmbeddingsType.PENULTIMATE_HIDDEN_STATES_NON_NORMALIZED,
             requires_pooled=[False, True],
         )
-        self.prompt_cache = FixedSizeDict(100)
+        self.prompt_cache = FixedSizeDict(32)
         print("Prepared compel")
 
         self.generator = torch.manual_seed(0)
@@ -78,16 +79,31 @@ class DiffusionProcessor:
     def embed_prompt(self, prompt):
         if prompt not in self.prompt_cache:
             with torch.no_grad():
+                print("embedding prompt", prompt)
                 self.prompt_cache[prompt] = self.compel(prompt)
         return self.prompt_cache[prompt]
-
+    
+    def meta_embed_prompt(self, prompt):
+        pattern = r'\("(.*?)"\s*,\s*"(.*?)"\)\.blend\((.*?),(.*?)\)'
+        match = re.search(pattern, prompt)
+        if not match:
+            return self.embed_prompt(prompt)
+        str1, str2, t1, t2 = match.groups()
+        t1 = float(t1)
+        t2 = float(t2)
+        cond1, pool1 = self.embed_prompt(str1)
+        cond2, pool2 = self.embed_prompt(str2)
+        cond = cond1 * t1 + cond2 * t2
+        pool = pool1 * t1 + pool2 * t2
+        return cond, pool
+    
     def run(self, images, prompt, num_inference_steps, strength, use_compel=False, seed=None):
         strength = min(max(1 / num_inference_steps, strength), 1)
         if seed is not None:
             self.generator = torch.manual_seed(seed)
         kwargs = {}
         if use_compel:
-            conditioning, pooled = self.embed_prompt(prompt)
+            conditioning, pooled = self.meta_embed_prompt(prompt)
             batch_size = len(images)
             conditioning_batch = conditioning.expand(batch_size, -1, -1)
             pooled_batch = pooled.expand(batch_size, -1)
